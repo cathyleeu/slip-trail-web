a01ae87c1a0a2cd364db7155dd3e502cb73bcae1
Cathy <leeyu823@gmail.com>
2025-12-12 11:55:54 -0800

    add: useAuth logic

--- app/hooks/useAuth.ts
+++ app/hooks/useAuth.ts
@@ -1,9 +1,33 @@
 'use client'
-
 import { useAuthContext } from '@context/AuthContext'
+import { supabaseClient } from '@lib/supabase/client'
 
 export function useAuth() {
-  const ctx = useAuthContext()
-  if (!ctx) throw new Error('useAuth must be used within AuthProvider')
-  return ctx
+  const { user, session, loading } = useAuthContext()
+  const supabase = supabaseClient()
+
+  async function login(email: string, password: string) {
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    })
+    if (error) throw error
+  }
+
+  async function signup(email: string, password: string) {
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+    })
+    if (error) throw error
+  }
+
+  async function logout() {
+    const { error } = await supabase.auth.signOut()
+    if (error) throw error
+  }
+
+  if (!user && !session && loading === undefined)
+    throw new Error('useAuth must be used within AuthProvider')
+  return { user, session, loading, login, signup, logout }
 }
