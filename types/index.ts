2a2b3cd6c893ea37fca436c830b8abfa2147134d
Cathy <leeyu823@gmail.com>
2026-01-08 15:06:56 -0800

    feat: add ImageFormat type and export from index

--- types/index.ts
+++ types/index.ts
@@ -5,7 +5,7 @@ export type * from './profile'
 
 // Domain-based types (refactored from receiptAnalysis)
 export type * from './analysis'
+export type * from './image'
 export type * from './location'
 export type * from './ocr'
 export type * from './receipt'
-
