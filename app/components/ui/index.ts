82e386ed93e0d55f390dabc0691d587724019572
Cathy <leeyu823@gmail.com>
2026-02-05 22:51:33 -0800

    feat: create index file to export UI components and types

---
+++ app/components/ui/index.ts
@@ -0,0 +1,13 @@
+export { default as Button } from './Button'
+export type { ButtonProps, ButtonSize, ButtonVariant } from './Button'
+export { default as IconButton } from './IconButton'
+export type {
+  IconButtonProps,
+  IconButtonShape,
+  IconButtonSize,
+  IconButtonVariant,
+} from './IconButton'
+
+// Icons
+export * from './icons'
+export type { IconProps } from './icons'
