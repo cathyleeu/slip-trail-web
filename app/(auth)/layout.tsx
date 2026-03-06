1041b612d594efb4431efcbf88f5ee687a43f46b
Cathy <leeyu823@gmail.com>
2026-03-05 23:02:22 -0800

    remove: header

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,19 +1,11 @@
 'use client'
 
-import { Header } from '@components'
 import { cn } from '@utils/cn'
-import { usePathname } from 'next/navigation'
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname()
-
-  const isHideHeader = pathname === '/onboarding'
   return (
     <div className="h-screen flex flex-col justify-between">
-      {!isHideHeader && <Header title={''} showBack />}
-      <div className={cn('overflow-auto', isHideHeader ? 'h-screen' : 'h-[calc(100vh-57px)]')}>
-        {children}
-      </div>
+      <div className={cn('overflow-auto', 'h-screen')}>{children}</div>
     </div>
   )
 }
